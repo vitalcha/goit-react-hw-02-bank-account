57ada511368792dadc6aa5ae38b0b6237deb8665
vitalcha <vkrokhov@gmail.com>
2019-11-28 15:24:27 +0200

    ad build

---
+++ build/precache-manifest.3a204889e8dffa0b68a8e8cab7a2d026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd8c7d9cbe3869f322eb99c7b53f2ea",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "10da95418a917fd2824f",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "5a1169e673081386098f",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.a07621dd.chunk.css"
+  },
+  {
+    "revision": "10da95418a917fd2824f",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.c73ebcff.chunk.js"
+  },
+  {
+    "revision": "5a1169e673081386098f",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.fcc84a76.chunk.js"
+  },
+  {
+    "revision": "59b69276754240abb686",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.611f8d42.js"
+  }
+]);
